8220b51aa544c41d9583a4254ef6b67f8922899e
Danny Robinson <daniel@djrobinson.me>
2019-03-03 09:26:36 -0700

    Adding react-websocket

--- services/nudge_client/src/App.js
+++ services/nudge_client/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import TestSocket from './TestSocket'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <TestSocket />
       </div>
     );
   }
