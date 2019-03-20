6dab788b51aa591b1e3af508bbada1d05f2043c8
Danny Robinson <daniel@djrobinson.me>
2019-03-19 20:48:21 -0600

    Adding sample poloniex init json, debug sses

--- services/nudge_client/src/components/TestSocket.js
+++ services/nudge_client/src/components/TestSocket.js
@@ -12,7 +12,7 @@ class TestSocket extends React.Component {
   async componentDidMount() {
     var response = await fetch("http://localhost:6066/test");
     var body = await response.text();
-    console.log("What is body: ", body)
+    console.log("What is body: ", body);
     var es = new EventSource('http://localhost:6066/sse', {withCredentials: true});
     es.onmessage = event => {
       console.log("What is ES event? ", event)
